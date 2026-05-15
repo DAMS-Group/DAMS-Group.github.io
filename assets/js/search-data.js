6313cea1bf407d407dc4e7a16c205382e77cf2c2
qianfei11 <33920374+qianfei11@users.noreply.github.com>
2026-05-15 03:01:36 +0000

    Deploying to gh-pages from @ DAMS-Group/DAMS-Group.github.io@4ba1e520eb5635df76875946fc8dd0c37260e813 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,6 +422,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/accept@dsn2026/";
+            },},{id: "news-论文录用-usenix-security-2026",
+          title: '【论文录用】USENIX Security 2026',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/accept@usenixsecurity2026-gaozibo/";
             },},{
         id: 'social-github',
         title: 'GitHub',
